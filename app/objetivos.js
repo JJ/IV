33c6f7682af21a29349e7857229660bdd6dc2626
JJ Merelo <jjmerelo@gmail.com>
2013-11-04 12:37:20 +0100

    Añade documentación

--- app/objetivos.js
+++ app/objetivos.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
+/* Script para bajarse los objetivos del día.
+
+Instalación: 
+
+sudo apt-get install node
+sudo cp objetivos.js /usr/local/bin
+cd /usr/local/bin
+npm install request
+npm install cheerio
+chmod +x objetivos.js
+
+Uso:
+objetivos.js 21102013 (para la clase que tenga esa fecha original)
+
+*/
+
 var date=process.argv[2];
 
 var URL="https://github.com/IV-GII/GII-2013/wiki/Clasedel"+date;
