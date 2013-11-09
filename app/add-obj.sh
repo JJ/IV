8188ac5b853e54c4336e814186cd808fb433ef40
JJ Merelo <jjmerelo@gmail.com>
2013-11-09 14:16:22 +0100

    Probando arrays s:11112013 o:3

--- app/add-obj.sh
+++ app/add-obj.sh
@@ -9,8 +9,12 @@ SESSION=`git log -1 --pretty=format:"%s" | grep -o 's:[[:digit:]]*\>' `
 SESSION=${SESSION/s:/}
 OBJ=`git log -1 --pretty=format:"%s" | grep -o 'o:[[:digit:]]*\>'`
 OBJ=${OBJ/o:/}
-SESSION_OBJS=( $(wget http://objiv-jmerelo.rhcloud.com/get/$SESSION -q -O -)) 
-echo $SESSION_OBJS[0]
+echo $SESSION
+declare -a SESSION_OBJS
+SESSION_OBJS=( $(wget http://objiv-jmerelo.rhcloud.com/get/$SESSION -q -O - ) )  
+echo $SESSION_OBJS
+echo ${SESSION_OBJS[@]}
+echo ${SESSION_OBJS[0]}
 
 echo -e $FILE
 echo $COMMIT_MESSAGE
