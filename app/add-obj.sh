a45569e6644758d8c78059e682e0f5a3a8522e03
JJ Merelo <jjmerelo@gmail.com>
2013-11-09 14:09:17 +0100

    Probando filtros s:8112013 o:3

--- app/add-obj.sh
+++ app/add-obj.sh
@@ -4,8 +4,13 @@ FILE=''
 while read LINE; do
     FILE+="${LINE}\n"
 done
-COMMIT_MESSAGE=`git log -1 --pretty=format:"%s" | grep -o 's:[[:digit:]]*\> `
-
+echo "Read file"
+SESSION=`git log -1 --pretty=format:"%s" | grep -o 's:[[:digit:]]*\>' `
+SESSION=${SESSION/s:/}
+OBJ=`git log -1 --pretty=format:"%s" | grep -o 'o:[[:digit:]]*\>'`
+OBJ=${OBJ/o:/}
+SESSION_OBJS=( $(wget http://objiv-jmerelo.rhcloud.com/get/$SESSION -q -O -)) 
+echo $SESSION_OBJS[0]
 
 echo -e $FILE
 echo $COMMIT_MESSAGE
