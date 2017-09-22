86fa3be89fc005770f100bf381070b26a5dc4ced
JJ Merelo <jjmerelo@gmail.com>
2017-09-22 10:54:44 +0200

    Creado para copiar hitos

---
+++ scripts/copia-hitos.pl
@@ -0,0 +1,28 @@
+#1/usr/bin/env perl
+
+use strict;
+use warnings;
+use File::Slurper qw(read_lines);
+
+use v5.14;
+
+my $file_name = shift || "hitos-0.md";
+
+my @lines = read_lines($file_name);
+
+die "No va el fichero" if !@lines;
+
+for my $l (@lines) {
+  if ( $l !~ /\|/ ) {
+    say $l;
+  } else {
+    if ( $l =~ /Nombre/) {
+      say $l;
+    } elsif ( $l =~ /----/ ) {
+      say $l;
+    } else {
+      my ($zipi,$nombre,@zape) = split(/\|/, $l);
+      say "|$nombre|", " |" x @zape ;
+    }
+  }
+}
