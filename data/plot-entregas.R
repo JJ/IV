5c6b68a487a4d942bf251eaac099aaaab54faefe
JJ Merelo <jjmerelo@gmail.com>
2019-11-08 21:20:43 +0100

    Actualiza datos y script

--- data/plot-entregas.R
+++ data/plot-entregas.R
@@ -5,4 +5,6 @@ data <- read.csv("notas-suspensos-entregas-2015-2019.csv")
 
 
 data <- data %>% group_by(Curso) %>% mutate(Porcentaje = Cuantos/max(Cuantos))
-ggplot( data,aes(x=Hito,y=Porcentaje,color=Curso,group=Curso)) + geom_line() 
+data$Curso <- as.factor(data$Curso)
+ggplot( data,aes(x=Hito,y=Porcentaje,color=Curso,group=Curso)) + geom_line() + theme_solarized()
+
