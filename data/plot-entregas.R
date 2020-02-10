ccf18a080305f9a80bc2b6c3f38bce43332f0d04
JJ Merelo <jjmerelo@gmail.com>
2020-02-10 13:24:36 +0100

    Script mejorado

--- data/plot-entregas.R
+++ data/plot-entregas.R
@@ -5,6 +5,7 @@ data <- read.csv("notas-suspensos-entregas-2015-2019.csv")
 
 
 data$Curso <- as.factor(data$Curso)
+data[is.na(data)] <- 0
 data$Aprobados <- data$Cuantos - data$Suspensos
 data <- data %>% group_by(Curso) %>% mutate(Porcentaje = Aprobados/max(Cuantos))
 ggplot( data,aes(x=Hito,y=Porcentaje,color=Curso,group=Curso)) + geom_line() + geom_point() + theme_solarized()
