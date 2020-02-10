b4355ead6e1a3dd0ee2040f830a947f38029d9c3
JJ Merelo <jjmerelo@gmail.com>
2020-02-10 13:20:25 +0100

    Traza aprobados en vez de entregas

--- data/plot-entregas.R
+++ data/plot-entregas.R
@@ -4,7 +4,8 @@ library(ggthemes)
 data <- read.csv("notas-suspensos-entregas-2015-2019.csv")
 
 
-data <- data %>% group_by(Curso) %>% mutate(Porcentaje = Cuantos/max(Cuantos))
 data$Curso <- as.factor(data$Curso)
+data$Aprobados <- data$Cuantos - data$Suspensos
+data <- data %>% group_by(Curso) %>% mutate(Porcentaje = Aprobados/max(Cuantos))
 ggplot( data,aes(x=Hito,y=Porcentaje,color=Curso,group=Curso)) + geom_line() + geom_point() + theme_solarized()
 
