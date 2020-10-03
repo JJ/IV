4b353143b54d31fbe833418a1f32829721f3e35b
JJ Merelo <jjmerelo@gmail.com>
2020-10-03 18:47:40 +0200

    Arregla conflictos closes #104

--- documentos/seminarios/code/jauja.rb
+++ documentos/seminarios/code/jauja.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env ruby
 
 puts "Esto es jauja"
-=======
-#!/usr/bin/ruby
-
-puts "Esto es jauja"
->>>>>>> 3bd00d4f0b77bb8b520e874d7ea3febf1d4bf23b
--- documentos/seminarios/code/osl-titulares.rb
+++ documentos/seminarios/code/osl-titulares.rb
@@ -7,11 +7,7 @@ require 'open-uri'
 osl =  Hpricot(open("http://osl.ugr.es/"))
 osl.search("//h2[@class='entry-title']").each { |h2|
   this_h2 = Hpricot( h2.to_s )
-<<<<<<< HEAD
   link = this_h2.search("a[@href]").text
-=======
-  link = this_h2.search("a[@href]").inner_html
->>>>>>> 3bd00d4f0b77bb8b520e874d7ea3febf1d4bf23b
   text = this_h2.at("a").inner_html
   puts "#{link} => #{text}"
 }
