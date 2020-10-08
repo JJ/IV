7c8562332a366be5b57a26cc6636fecbbdb1a2d0
lulivi <luislivilla@gmail.com>
2020-10-08 23:24:36 +0200

    Add explanation to mld configuration
    Define a fixed header style across the repository.
    
    Related #85

--- .mdl.rb
+++ .mdl.rb
@@ -1,3 +1,6 @@
+# Use all the rules by default
 all
+# Allow some punctuation in the header, as :
 rule 'MD026', :punctuation => '.,;!?'
-
+# Enforce classic Markdown style headers
+rule 'MD003', :style => :atx
