adc2d2ae0c7cfad9003f24579e541c73803ccd74
lulivi <luislivilla@gmail.com>
2020-10-09 17:20:33 +0200

    Configure ordered list prefix style rule
    Related #85

--- .mdl.rb
+++ .mdl.rb
@@ -4,3 +4,5 @@ all
 rule 'MD026', :punctuation => '.,;!?'
 # Enforce classic Markdown style headers
 rule 'MD003', :style => :atx
+# Enforce ordered list prefix style
+rule 'MD029', :style => :ordered
