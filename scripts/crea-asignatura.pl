da8ebd15fe32871a7e22051e4d3e0e60331aaf4c
JJ Merelo <jjmerelo@gmail.com>
2021-08-27 11:12:34 +0200

    [skip ci] Script (posiblemente inútil) para crear la asignatura...

--- scripts/crea-asignatura.pl
+++ scripts/crea-asignatura.pl
@@ -3,14 +3,16 @@
 use strict;
 use warnings;
 
-use File::Copy;
+use File::Copy; # Recuerda cpanm --installdeps .
+use File::Path qw(make_path);
+use Cwd;
 
 use v5.16;
 
 my $dir = shift || ".";
-my $previo = shift || "IV-19-20";
+my $previo = shift || "IV-20-21";
 
-for my $d ( qw( objetivos proyectos sesiones) ) {
+for my $d ( qw( proyectos sesiones) ) {
     eval {
 	mkdir("$dir/$d");
     };
@@ -20,7 +22,32 @@ for my $d ( qw( objetivos proyectos sesiones) ) {
     move( 'README.md', "$dir/$d/" ) || die "Can't write file";
 }
 
-mkdir("$dir/t");
-for my $f ( qw (.travis.yml .gitignore LICENSE Metodología_y_criterios_de_evaluación.md README.md cpanfile) ) {
+download_files_here( qw (.gitignore LICENSE Metodología_y_criterios_de_evaluación.md README.md cpanfile) );
+
+create_and_download("t", "check-hitos.t");
+
+create_and_download( ".github/workflows", qw( check-by-milestone.yaml   ficheros.yml  objetivos.yml check-usuario-objetivos.yaml  get-pr-checks.yml  procesa_yaml.sh) );
+
+create_and_download( "src", "get-config.pl" );
+
+sub download_files_here {
+  for my $f (  @_ ) {
     `wget  --backups=1 https://raw.githubusercontent.com/JJ/$previo/master/$f`;
+  }
+}
+
+sub download_files {
+  my $dir = shift;
+  for my $f (  @_ ) {
+    `wget  --backups=1 https://raw.githubusercontent.com/JJ/$previo/master/$dir/$f`;
+  }
+}
+
+sub create_and_download {
+  my $dir = shift;
+  my $former_dir = getcwd;
+  make_path( $dir );
+  chdir( $dir );
+  download_files( $dir, @_ );
+  chdir( $former_dir );
 }
