a9870a15066045a8c67d55c83f7cdfb35f1ffca7
JJ Merelo <jjmerelo@gmail.com>
2021-09-16 07:34:32 +0200

    Adapta a nuevo formato

--- app/git-hooks.pl
+++ app/git-hooks.pl
@@ -29,7 +29,7 @@ POST_COMMIT {
       $git->command( 'checkout', 'master', '--', $f );
       my $file_content = read_file( $f );
       $file_content =~ s/(?<!README)\.md\)/\)/g; # Change links
-      $file_content =~ s/(proyectos.hito-\d)/$1.md/g; # Reset links to repo
+      $file_content =~ s/(proyectos\/\w+-\d+)/$1.md/g; # Reset links to repo
       if ( $f =~ /practicas/ ) {
 	  $file_content =~ s{/(\d)}{/$1.md}g; # Change back for links to prácticas
       }
