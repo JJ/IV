8a0908bef5cb73406e3a3af0cf9bb357e000a752
JJ Merelo <jjmerelo@gmail.com>
2022-01-30 20:31:05 +0100

    Añade boxplot corrección

--- scripts/chart-fechas.R
+++ scripts/chart-fechas.R
@@ -10,3 +10,8 @@ datos$Entrega <- parse_iso_8601(datos$Entrega)
 datos$superacion <- (datos$Correccion - datos$Entrega)/86400;
 
 summary( datos$superacion )
+datos$Objetivo <- as.factor(datos$Objetivo)
+ggplot( datos, aes(x=Entrega,y=Objetivo))+geom_point()+theme_economist_white()
+ggplot( datos, aes(x=Objetivo, y=superacion,color=Objetivo))+ geom_boxplot()
+superados <- datos[ datos$Incompleto == "Completo",]
+ggplot( superados, aes(x=Objetivo, y=superacion,color=Objetivo))+ geom_boxplot() +theme_economist_white()
