170965499395878e4e7f732c079d94323e0cd45f
JJ Merelo <jjmerelo@gmail.com>
2022-01-30 20:21:47 +0100

    Añade entregas

--- scripts/chart-fechas.R
+++ scripts/chart-fechas.R
@@ -7,6 +7,6 @@ datos <- read.csv("../IV-21-22/data/fechas-entrega.csv", sep=";")
 datos$Correccion <- parse_iso_8601(datos$Correccion)
 datos$Entrega <- parse_iso_8601(datos$Entrega)
 
-datos$superacion <- datos$Correccion - datos$Entrega;
+datos$superacion <- (datos$Correccion - datos$Entrega)/86400;
 
 summary( datos$superacion )
