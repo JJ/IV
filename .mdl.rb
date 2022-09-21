a6b7039b887dd44f76f662e20df667604348926c
JJ Merelo <jjmerelo@gmail.com>
2022-09-21 20:46:44 +0200

    Altera las reglas de linting

--- .mdl.rb
+++ .mdl.rb
@@ -1,7 +1,7 @@
 # Use all the rules by default
 all
 # Allow some punctuation in the header, as :
-rule 'MD026', :punctuation => '.,;!?'
+rule 'MD026', :punctuation => '.,;'
 # Enforce classic Markdown style headers
 rule 'MD003', :style => :atx
 # Enforce ordered list prefix style
