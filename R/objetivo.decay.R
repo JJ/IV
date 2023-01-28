9c58aa2fdf8cc39fa5f0e552490819be2ac1fb67
JJ Merelo <jjmerelo@gmail.com>
2023-01-28 11:52:19 +0100

    Procesa datos

--- R/objetivo.decay.R
+++ R/objetivo.decay.R
@@ -1,4 +1,15 @@
+library(parsedate)
+
 objetivo.decay <- function( fichero, fecha.inicio ) {
   datos <- read.csv(fichero, sep=";")
   inicio <- parse_iso_8601(fecha.inicio)
+  datos$Correccion <- parse_iso_8601(datos$Correccion)
+  datos$Correccion.Semana <- as.numeric( datos$Correccion - inicio, units = "weeks")
+  
+  datos$Entrega <- parse_iso_8601(datos$Entrega)
+  datos$Entrega.Semana <- as.numeric( datos$Entrega - inicio, units = "weeks")
+  
+  datos$superacion <- as.numeric(datos$Correccion - datos$Entrega, units="days");
+  
+  return( datos )
 }
---
+++ scripts/decay.R
@@ -0,0 +1,3 @@
+source("R/objetivo.decay.R")
+
+datos <- objetivo.decay( "../IV-21-22/data/fechas-entrega.csv","2021-09-13T00:00:00+02:00")
