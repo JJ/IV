1a2a9b1186793b2240f54e248fdc4203821b918b
JJ Merelo <jjmerelo@gmail.com>
2025-09-08 12:20:08 +0200

    Tampoco están conectados a nada
    Ni tiene ejemplos del contenido actual

--- ejemplos/README.md
+++
@@ -1,3 +0,0 @@
-# Material docente para Infraestructura Virtual: Ejemplos
-
-Ejemplos en diferentes lenguajes de programación.
--- ejemplos/chef/solo.rb
+++
@@ -1,4 +0,0 @@
-#Configuration file for chef-solo
-file_cache_path "/home/jmerelo/Asignaturas/IV/ejemplos/chef"
-cookbook_path "/home/jmerelo/Asignaturas/IV/ejemplos/chef/cookbooks"
-json_attribs "/home/jmerelo/Asignaturas/IV/ejemplos/chef/node.json"
