647c53bbff3c3bf2c8691a827cd51966026a0521
JJ Merelo <jjmerelo@gmail.com>
2026-06-28 10:42:05 +0200

    Reconfigure rule

--- .mdl.rb
+++ .mdl.rb
@@ -9,6 +9,6 @@ rule 'MD029', :style => :ordered
 # Allow raw HTML for notes and other stuff
 exclude_rule 'MD033'
 # Allow long lines on code blocks only
-rule 'MD013', :ignore_code_blocks => true
+rule 'MD013', :ignore_code_blocks => true, :treat_links_as_single_words => true
 # Unordered list impossible to fix
 exclude_rule 'MD007'
